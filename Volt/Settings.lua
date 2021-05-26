f5a856767ebee8dd0cb9965103d44d6faa35a009
astrealRBLX <80359818+astrealRBLX@users.noreply.github.com>
2021-05-26 15:04:02 -0400

    Create Settings.lua

---
+++ Volt/Settings.lua
@@ -0,0 +1,14 @@
+--[[
+
+	Volt.Settings // Internal Settings
+	
+	Manages various volt settings pertaining
+	to your project
+	
+]]
+
+return {
+	LibrariesFolder = game:GetService('ReplicatedStorage').Volt.Libraries,
+	ServicesFolder = game:GetService('ReplicatedStorage').Volt.Services,
+	Debug = true
+}
